868e1391b60210924c441fbc645fef8f153eb8d2
LuizGuerra <luizpedrofg@gmail.com>
2021-03-11 20:30:31 -0300

    main script file

---
+++ src/mainScript.js
@@ -0,0 +1,6 @@
+// document.getElementById('tagged').style.borderColor = 'green';
+// document.getElementById('tagged').style.borderWidth = '3px';
+// document.getElementById('tagged').style.borderRadius = '10px';
+
+
+console.log('Hello, world!');
